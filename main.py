d638f7bb7fc4eeabcca6287cc66c8a13d0c54a66
micha <michoffmann.potsdam@gmail.com>
2018-02-18 11:25:45 +0100

    Added argparser

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-import sys
+import argparse
 import os
 
 from project.lib.JSONTree import JSONTree
 
-filepath = os.path.normpath(sys.argv[1])
+parser = argparse.ArgumentParser(description='Sample JSON Formatter')
+
+parser.add_argument('-htmlize', help="Convert JSON at path into a nested HTML Table",
+                    action="store_true", dest="htmlize", default=False)
+parser.add_argument('-flatten', help="Convert JSON at path into a flattened Key Value List",
+                    action="store_true", dest="flatten", default=False)
+
+parser.add_argument('-path', help="Specify the path to the JSON File", action="store", dest="path")
+
+arguments = parser.parse_args()
+
+filepath = os.path.normpath(arguments.path)
 
 with open(filepath, 'r') as f:
     json_tree = JSONTree.from_string(f.read())
-    print(json_tree.htmlize())
-    print(json_tree.flatten())
+    if arguments.htmlize:
+        print(json_tree.htmlize())
+    if arguments.flatten:
+        print(json_tree.flatten())
